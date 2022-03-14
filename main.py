5724be6bea478ec8628bb602193e408e8c956505
LuckyPotato228 <91941548+LuckyPotato228@users.noreply.github.com>
2022-03-14 10:18:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-from math import ceil, floor
+from math import ceil,floor
+count = 0
+num1 = int(25317)
+num2 = int(51237)
 def is_prime(x):
     for i in range(2,ceil(x**0.5)+1):
-        if x % i == 0:
+        if x%i == 0:
             return False
-        return True
-count = 0
-first = int (input())
-second = int(input())
-Luke = [3,2]
-for i in range(2,1000):
-    if Luke[i-1]+Luke[i-2] > second:
-        break
-    Luke+= [Luke[i-1]+ Luke[i-2]]
-
-for j in range(len(Luke)):
-    if Luke[j]>first and Luke[j]<second and is_prime(Luke[j])== True:
-            print(j+1)
-            print(Luke[j])
-            count+=1
-print(count)
-print(*Luke)
+    return True
+def divisors(x):
+    divs=[1,x]
+    if x ** 0.5 == int(x**0.5):
+        divs.append(int(x**0.5))
+    for i in range(2,ceil(x**0.5)):
+        if x%i == 0:
+            divs.append(i)
+            divs.append(x//i)
+    return sorted(divs)
+for i in range(num1,num2):
+    d = divisors(i)
+    if len(d)>=8:
+        for j in range(len(d)):
+            ans = is_prime(d[j])
+            if ans == True:
+                count+=1
+        if count>=7:
+            print(d)
