5220c8948c274133a39dcc84d699cdcc7d0f4414
LuckyPotato228 <91941548+LuckyPotato228@users.noreply.github.com>
2022-03-17 02:25:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
-from math import ceil,floor
-count = 0
+from math import ceil, floor
+
+
 num1 = int(25317)
 num2 = int(51237)
+
+
 def is_prime(x):
-    for i in range(2,ceil(x**0.5)+1):
-        if x%i == 0:
+    for i in range(2, ceil(x ** 0.5) + 1):
+        if x % i == 0:
             return False
     return True
+
+
 def divisors(x):
-    divs=[1,x]
-    if x ** 0.5 == int(x**0.5):
-        divs.append(int(x**0.5))
-    for i in range(2,ceil(x**0.5)):
-        if x%i == 0:
+    divs = [1, x]
+    if x ** 0.5 == int(x ** 0.5):
+        divs.append(int(x ** 0.5))
+    for i in range(2, ceil(x ** 0.5)):
+        if x % i == 0:
             divs.append(i)
-            divs.append(x//i)
+            divs.append(x // i)
     return sorted(divs)
-for i in range(num1,num2):
-    d = divisors(i)
-    if len(d)>=8:
-        for j in range(len(d)):
-            ans = is_prime(d[j])
-            if ans == True:
-                count+=1
-        if count>=7:
-            print(d)
+
+
+def eratosphenes(upto):
+    upto += 1
+    cand = list(range(upto))
+    for seed in range(2, ceil(upto ** 0.5) + 1):
+        if cand[seed] != 0:
+            for i in range(seed * seed, upto, seed):
+                cand[i] = 0
+    primes = [i for i in cand if i > 1]
+    return primes
+
+
+def sum_of_num(x):
+    num = int(x)
+    sum = 0
+    while (num != 0):
+        sum = sum + num % 10
+        num = num // 10
+    return sum
+
+
+def algorythm_1(x,y,z):
+    a = list(eratosphenes(y))
+    for i in range(len(a)):
+        if x < a[i] < y and sum_of_num(a[i]) > z:
+            print(a[i])
+
+def algorythm_2(num1,num2):
+    for i in range(num1,num2):
+        count = 0
+        d = divisors(i)
+        if len(d)>=8:
+            for j in range(len(d)):
+                ans = is_prime(d[j])
+                if ans == True:
+                    count+=1
+            if count>=6:
+                print(i,d)
+algorythm_2(25317,51237)
