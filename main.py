c47e7bf840bf55260415358c0c63a07bbf6b7b26
Егор Арепьев <bruhsusamogus@omegalul.com>
2022-03-17 13:38:47 +0300

    bruh

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from math import ceil, floor
 
 
-num1 = int(25317)
-num2 = int(51237)
-
-
 def is_prime(x):
-    for i in range(2, ceil(x ** 0.5) + 1):
-        if x % i == 0:
-            return False
+    if x != 2:
+        for i in range(2, ceil(x ** 0.5) + 1):
+            if x % i == 0:
+                return False
     return True
 
 
@@ -51,13 +48,39 @@ def algorythm_1(x,y,z):
 
 def algorythm_2(num1,num2):
     for i in range(num1,num2):
+        prime_nums = []
         count = 0
         d = divisors(i)
         if len(d)>=8:
             for j in range(len(d)):
                 ans = is_prime(d[j])
                 if ans == True:
+                    prime_nums += [d[j]]
                     count+=1
-            if count>=6:
+            if count>=7:
                 print(i,d)
-algorythm_2(25317,51237)
+                print(prime_nums)
+
+
+def algorythm_3(num1,num2,step):
+    for i in range(num1,num2):
+        d = divisors(i)
+        count = 0
+        for j in range(len(d)-1):
+            if j>1:
+                if d[j] == d[j-1]+step:
+                    count+=1
+            if len(d) > 3 and count == len(d)-2:
+                print(i)
+                print(d)
+
+
+def algorythm_4():
+    pass
+
+
+def algorythm_5():
+    pass
+
+
+algorythm_3(854321,1087654,10)
